fbc7220879824ea9c6f4dbc3fa2750527d6027c1
Shash112 <126497800+Shash112@users.noreply.github.com>
2024-11-21 01:10:37 +0530

    footer change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='bg-slate-300'>
       <div className='container mx-auto p-4'>
-        <p className='text-center font-bold'>Code with Shashank ❤️</p>
+        <p className='text-center font-bold'>Developed by PRAJNAN SOFTWARE ❤️</p>
       </div>
     </footer>
   )
