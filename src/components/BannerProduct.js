5f856e1ab3e56296fe427cca1867d7e5775012e0
Shash112 <126497800+Shash112@users.noreply.github.com>
2024-12-01 19:59:39 +0530

    banner updated

--- src/components/BannerProduct.js
+++ src/components/BannerProduct.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import image1 from '../assest/assest/banner/img1.webp'
-import image2 from '../assest/assest/banner/img2.webp'
-import image3 from '../assest/assest/banner/img3.jpg'
-import image4 from '../assest/assest/banner/img4.jpg'
-import image5 from '../assest/assest/banner/img5.webp'
+import image1 from '../assest/assest/banner/image1.jpg'
+import image2 from '../assest/assest/banner/image2.jpg'
+import image3 from '../assest/assest/banner/image3.jpg'
+import image4 from '../assest/assest/banner/image4.jpg'
+import image5 from '../assest/assest/banner/image5.jpg'
 
 
-import image1Mobile from '../assest/assest/banner/img1_mobile.jpg'
-import image2Mobile from '../assest/assest/banner/img2_mobile.webp'
-import image3Mobile from '../assest/assest/banner/img3_mobile.jpg'
-import image4Mobile from '../assest/assest/banner/img4_mobile.jpg'
-import image5Mobile from '../assest/assest/banner/img5_mobile.png'
+// import image1Mobile from '../assest/assest/banner/img1_mobile.jpg'
+// import image2Mobile from '../assest/assest/banner/img2_mobile.webp'
+// import image3Mobile from '../assest/assest/banner/img3_mobile.jpg'
+// import image4Mobile from '../assest/assest/banner/img4_mobile.jpg'
+// import image5Mobile from '../assest/assest/banner/img5_mobile.png'
 
 import { FaAngleRight } from "react-icons/fa6";
 import { FaAngleLeft } from "react-icons/fa6";
@@ -20,7 +20,7 @@ const BannerProduct = () => {
 
     const desktopImages = [image1, image2, image3, image4, image5]
 
-    const mobileImages = [image1Mobile, image2Mobile, image3Mobile, image4Mobile, image5Mobile]
+    // const mobileImages = [image1Mobile, image2Mobile, image3Mobile, image4Mobile, image5Mobile]
 
     const nextImage = () => {
         if(desktopImages.length - 1 > currentImage){
@@ -48,7 +48,7 @@ const BannerProduct = () => {
 
   return (
     <div className='container mx-auto px-4 rounded'>
-        <div className='h-56 md:h-72 w-full bg-slate-200 relative'>
+        <div className='h-auto md:h-72 w-full bg-slate-200 relative'>
             <div className='absolute z-10 h-full w-full md:flex items-center hidden'>
                 <div className='flex justify-between w-full text-2xl'>
                     <button onClick={prevImage} className='bg-white shadow-md rounded-full p1'><FaAngleLeft/></button>
@@ -56,19 +56,19 @@ const BannerProduct = () => {
                 </div>
             </div>
 
-            <div className='hidden md:flex h-full w-full overflow-hidden'>
+            <div className='flex h-full w-full overflow-hidden'>
                 {
                     desktopImages.map((imageURL,index) => {
                         return (
                             <div className='w-full h-full min-w-full transition-all' key={imageURL} style={{transform: `translateX(-${currentImage * 100}%)`}}>
-                                <img src={imageURL} className='w-full h-full'/>
+                                <img alt='banner' src={imageURL} className='w-full h-full object-contain md:object-cover'/>
                             </div>
                         )
                     })
                 }
             </div>
 
-            <div className='flex h-full w-full overflow-hidden md:hidden'>
+            {/* <div className='flex h-full w-full overflow-hidden md:hidden'>
                 {
                     mobileImages.map((imageURL,index) => {
                         return (
@@ -78,7 +78,7 @@ const BannerProduct = () => {
                         )
                     })
                 }
-            </div>
+            </div> */}
         </div>
     </div>
   )
