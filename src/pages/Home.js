0cae0a0e276dfd0c299fd01a556011aecad85b2b
Shash112 <126497800+Shash112@users.noreply.github.com>
2024-12-01 20:14:45 +0530

    done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,15 +10,16 @@ const Home = () => {
       <CategoryList />
       <BannerProduct />
 
-      <HorizontalCardProduct category={"airpodes"} heading={"Top Airpodes"}/>
-      <VerticalCardProduct category={"mobiles"} heading={"Mobiles"}/>
-      <HorizontalCardProduct category={"watches"} heading={"Popular Watches"}/>
+      <HorizontalCardProduct category={"mobiles"} heading={"Top Mobiles"}/>
+      <VerticalCardProduct category={"airpodes"} heading={"Airpodes"}/>
+      <HorizontalCardProduct category={"refrigerator"} heading={"Popular Refrigerators"}/>
+      <HorizontalCardProduct category={"camera"} heading={"New Cameras"}/>
       <VerticalCardProduct category={"mouse"} heading={"Mouse"}/>
       <VerticalCardProduct category={"televisions"} heading={"Televisions"}/>
       <VerticalCardProduct category={"camera"} heading={"Camera"}/>
       <VerticalCardProduct category={"earphones"} heading={"Earphones"}/>
       <VerticalCardProduct category={"speakers"} heading={"Speakers"}/>
-      <VerticalCardProduct category={"refrigerator"} heading={"Refrigerator"}/>
+      <VerticalCardProduct category={"watches"} heading={"Watches"}/>
       <VerticalCardProduct category={"trimmers"} heading={"Trimmers"}/>
     </div>
   )
